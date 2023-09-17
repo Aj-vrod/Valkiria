d8828ea8fdce0c19622a5c877e1576fa39d546e3
Aj-vrod <ammyvrodriguez@hotmail.com>
2023-09-17 18:53:22 +0200

    add /movie endpoint and return movie options according to genre

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -20,10 +20,42 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, user!\n")
 }
 
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /movie request\n")
+	genre := r.URL.Query().Get("genre")
+	resp := "Movie of today is "
+	w.WriteHeader(http.StatusOK)
+	var movie = "Matrix"
+	switch genre {
+	case "horror":
+		movie = "Saw"
+	case "comedy":
+		movie = "What happens in Vegas"
+	case "action":
+		movie = "Die Hard"
+	case "thriller":
+		movie = "The Silence of the Lambs"
+	case "drama":
+		movie = "The green mile"
+	case "fantasy":
+		movie = "The Lord of the Rings"
+	case "sci-fi":
+		movie = "The Matrix"
+	case "adventure":
+		movie = "The Hobbit"
+	case "western":
+		movie = "The Good, the Bad and the Ugly"
+	case "crime":
+		movie = "The Godfather"
+	}
+	io.WriteString(w, resp+movie+"\n")
+}
+
 func StartServer() {
 	fmt.Println("Starting server...")
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/movie", getMovie)
 
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
