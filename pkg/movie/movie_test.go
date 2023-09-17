b4bab76f2c0bf68c09cab36b49fd267cbd2173ca
Aj-vrod <ammyvrodriguez@hotmail.com>
2023-09-17 20:05:28 +0200

    add test for pickMovie function

---
+++ pkg/movie/movie_test.go
@@ -0,0 +1,17 @@
+package movie
+
+import "testing"
+
+var genres = []string{"horror", "comedy", "drama", "fantasy", "sci-fi", "thriller", "action", "adventure", "western", "crime"}
+var movies = []string{"Saw", "What happens in Vegas", "The green mile", "The Lord of the Rings", "The Matrix", "The Silence of the Lambs", "Die Hard", "The Hobbit", "The Good, the Bad and the Ugly", "The Godfather"}
+
+func TestPickMovie(t *testing.T) {
+	for i, v := range genres {
+		got := PickMovie(v)
+		exp := movies[i]
+
+		if got != exp {
+			t.Errorf("got %s, expected %s", got, exp)
+		}
+	}
+}
