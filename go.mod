1e67814a6ea3c1ca225a23971665137cc9840a6b
Aj-vrod <ammyvrodriguez@hotmail.com>
2023-11-03 21:46:12 +0100

    move db credentials to .env file

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/Valkiria
 go 1.20
 
 require (
+	github.com/Valgard/godotenv v0.0.0-20200511222744-8873b92a09c5
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/Valgard/go-pcre v0.0.0-20200510215507-235e400e25e9 // indirect
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,21 +3,27 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	"github.com/Valgard/godotenv"
 )
 
 var DB *sql.DB
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password01"
-	dbname   = "valkiria"
-)
-
 func InitDB() error {
+	dotenv := godotenv.New()
+	if err := dotenv.Load(".env"); err != nil {
+		return err
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	var err error
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	DB, err = sql.Open("postgres", psqlInfo)
